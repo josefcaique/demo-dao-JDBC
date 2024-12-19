086d86e059c78bb8f00352b6ae1cc94261c74d94
josefcaique <jcaique10@gmail.com>
2024-12-18 23:30:21 -0300

    Adding class seller

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 import entities.Department;
+import entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
 
         Department obj = new Department(1, "Books");
-        System.out.println(obj);
+        Seller seller = new Seller(21, "Maria", "maria@gmail.com", new Date(),3000.0, obj);
+        System.out.println(seller);
     }
 }
