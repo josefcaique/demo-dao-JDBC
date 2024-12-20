c3e3e7e7e826389fac8b6baffcdea7f4d9f02f38
josefcaique <jcaique10@gmail.com>
2024-12-19 22:49:29 -0300

    creating sellerDaoJdbc and DaoFactory

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import dao.DaoFactory;
+import dao.SellerDao;
 import entities.Department;
 import entities.Seller;
 
@@ -8,6 +10,7 @@ public class Main {
 
         Department obj = new Department(1, "Books");
         Seller seller = new Seller(21, "Maria", "maria@gmail.com", new Date(),3000.0, obj);
+        SellerDao sellerDao = DaoFactory.createSellerDao();
         System.out.println(seller);
     }
 }
