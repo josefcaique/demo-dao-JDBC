363a4568d0796c1fe04074f7daddc0d220a7bba8
josefcaique <jcaique10@gmail.com>
2024-12-22 00:39:36 -0300

    adding update statment

--- src/Main.java
+++ src/Main.java
@@ -30,7 +30,13 @@ public class Main {
 
         System.out.println("\n=== TEST 4: seller insert ===");
         Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0,department);
-        sellerDao.insert(newSeller);
-        System.out.println("Inserted! New ID = " + newSeller.getId());
+        //sellerDao.insert(newSeller);
+        //System.out.println("Inserted! New ID = " + newSeller.getId());
+
+        System.out.println("\n=== TEST 5: seller update ===");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Wayne");
+        sellerDao.update(seller);
+        System.out.println("update Completed");
     }
 }
